526721627eb4344ffe46cb6c53a42ae807c8be40
Your Name <go2starr@gmail.com>
2012-04-24 16:46:38 -0500

    PWM code bitches

---
+++ arduino/ThreeChannelPWM/ThreeChannelPWM.ino
@@ -0,0 +1,14 @@
+
+
+/* Setup - Initialize PWM registers */
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+/* Loop - update PWM counters */
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+}
